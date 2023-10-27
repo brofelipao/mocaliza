a03ba462c3fd5f7f297e2e6d64867f88c16b566d
unknown <ra177210@ACAD.UCDB.BR>
2023-10-26 20:35:35 -0400

    alteracoes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import express from 'express'; // Carrega a biblioteca dentro da variável express
+import express from 'express';
 import sqlite3 from 'sqlite3';
 import { Category } from './models/category.js';
 
@@ -6,39 +6,38 @@ const db = new sqlite3.Database('mocaliza_db.db');
 const app = express(); // Ao Carregar a biblioteca, o express cria uma função. Ao chamar a 
                        //função e atribuir para app, é criada uma instancia da biblioteca
 
-db.serialize(() => {
-    db.run(`CREATE TABLE IF NOT EXISTS categories (
-        id INTEGER NOT NULL,
-        name VARCHAR(255) NOT NULL,
-        updatedAt TIMESTAMP,
-        createdAt TIMESTAMP,
-        active BOOLEAN,
-        CONSTRAINT categories_pk PRIMARY KEY (category_id)
-    );`);
-    db.run(`INSERT INTO categories(name) VALUES ('CARROS')`);
-    db.run(`INSERT INTO categories(name) VALUES ('MOTOS')`);
-    db.run(`INSERT INTO categories(name) VALUES ('TRATORES')`);
-    db.run(`INSERT INTO categories(name) VALUES ('SUV')`);
-    db.run(`INSERT INTO categories(name) VALUES ('MOTO')`);
-});
+// db.serialize(() => {
+//     db.run(`CREATE TABLE IF NOT EXISTS categories (
+//         id INTEGER NOT NULL,
+//         name VARCHAR(255) NOT NULL,
+//         updatedAt TIMESTAMP,
+//         createdAt TIMESTAMP,
+//         active BOOLEAN,
+//         CONSTRAINT categories_pk PRIMARY KEY (id)
+//     );`);
+//     db.run(`INSERT INTO categories(name) VALUES ('CARROS')`);
+//     db.run(`INSERT INTO categories(name) VALUES ('MOTOS')`);
+//     db.run(`INSERT INTO categories(name) VALUES ('TRATORES')`);
+//     db.run(`INSERT INTO categories(name) VALUES ('SUV')`);
+//     db.run(`INSERT INTO categories(name) VALUES ('MOTO')`);
+// });
 
 app.get('/', (request, response) => {
-    var categories = []
+    const categories = new Array();
     db.serialize(() => {
-        db.each(`SELECT id, nome from categories`, (err, row) => {
+        db.each(`SELECT * from categories`, (err, row) => {
             if (err){
                 throw err;
             }
-            var category = new Category(id = row.id, name = row.name);
-            categories.push(row);
+            var category = new Category(row.id, row.name, row.active, row.createdAt, row.updatedAt);
+            categories.push(category);
+            console.log(category);
         })
-
-        console.log(categories);
-    });
-
-    response.json({
-        date: Date.now()
-    });
+        
+        response.json({
+            categorias: categories
+        });
+    })
 });
 
 // Função executada ao iniciar o servidor
